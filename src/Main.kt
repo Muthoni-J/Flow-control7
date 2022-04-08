86938571ee7ae8cf5a09effeeae12ab84035aab6
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-04-08 09:03:24 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,68 @@
+fun main(){
+    oddnumbers()
+    var names = peoplesnames(arrayOf("Gumato", "Moureen", "Charity", "Joan"))
+        println(names)
+    guestdrinks(8)
+    multiplenums()
+}
+
+//Create a function that prints out all the odd numbers between 1 and 100 (2pts)
+fun oddnumbers(){
+    for(num in 1..100)
+      if(num  %2 !=0){
+        println(num)
+    }
+
+}
+//Create a function that takes in an array of names and returns the number of
+//names longer than 5 characters (2pt
+fun peoplesnames(name:Array<String>):Int{
+    var y=0
+    for (n in name){
+        if (n.length>5){
+            y++
+        }
+    }
+    return y
+}
+
+//You are creating a robot to serve drinks at a party. The robot is required to
+//serve a glass of milk for guests under the age of 6. For guests under the age of
+//15 but over the age of 6, the robot serves a bottle of fanta orange. Everybody
+//else gets a bottle of coca cola. Create a function that will make these decisions,
+//printing out the name of the drink each person gets according to their age.2pts
+
+fun guestdrinks(age:Int) {
+    if (age<6) {
+        println("Glass of milk")
+    }
+    else if(age >6 && age <15) {
+        println("Bottle of fanta orange")
+    }
+        else {println("Bottle of coca cola")
+    }
+}
+
+//Write a function that prints each number from 1 to 100 on a new line. For each
+//multiple of 3, print "Fizz" instead of the number. For each multiple of 5, print
+//"Buzz" instead of the number. For numbers which are multiples of both 3 and
+//5, print "FizzBuzz" instead of the number. (4p
+fun multiplenums() {
+    for (num in 1..100) {
+        if (num % 3 == 0 && num % 5 == 0){
+            println("FizzBuzz")
+    }
+
+        else if (num%3==0) {
+            println("Fizz")
+        }
+        else if (num%5==0) {
+            println("Buzz")
+        }
+        else {
+            println(num)
+        }
+    }
+
+}
+
